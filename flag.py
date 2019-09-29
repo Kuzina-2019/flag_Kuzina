12f3e811fe5f1583cb09ceae2db1caa46d5c2a7b
Kuzina-2019 <kuzinaov@yandexlyceum.ru>
2019-09-29 21:04:24 +0300

    нарисован флагшток

--- flag.py
+++ flag.py
@@ -11,7 +11,16 @@ def draw1():
     rect_point = [(20, 10), (30, 280)]
     pygame.draw.rect(screen, rect_color, rect_point, rect_width)
 
+def draw2():
+    rect_color = pygame.Color("white")
+    rect_width = 0
+    rect_point = [(50, 10), (200, 50)]
+    pygame.draw.rect(screen, rect_color, rect_point, rect_width)
+
+
 draw1()
+draw2()
+
 
 while pygame.event.wait().type != pygame.QUIT:
     pygame.display.flip()
